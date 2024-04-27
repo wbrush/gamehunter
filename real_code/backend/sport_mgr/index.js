15397e55303cd71cd03a69214a4fcf1eca814032
Wayne Brush <wbrush@austin.rr.com>
2024-04-26 22:40:59 -0500

    adding example of fully qualified endpoint

--- real_code/backend/sport_mgr/index.js
+++ real_code/backend/sport_mgr/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`)
@@ -10,6 +10,19 @@ app.listen(port,()=>{
 
 app.get("/",(req,res)=>{
     return res.status(200).json({message : "Cloud Run and Cloud Build Demo - CI CD Pipeline"})
+})
+
+app.get("/api/v1/healthz",(req,res)=>{
+    acceptHeader = req.header('Accept')
+    if (acceptHeader.includes('json')) {
+        res.status(200).json({status : "Ok"})
+    } else if (acceptHeader.includes('plain')) {
+        res.set('Content-Type', 'text/html');
+        res.status(200).send("Health: Ok");
+    } else {
+        res.status(412).json({error : "Invalid Accept Header"})
+    }
+    return
 })
 
 app.get("/echo/:text",(req,res)=>{
