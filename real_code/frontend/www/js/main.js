2e0d52e327d4a5db64438d18ee509489635440ad
DBrush <coldnebraska@gmail.com>
2024-04-29 09:50:16 -0500

    added functions for dynamically adding data to the html

--- real_code/frontend/www/js/main.js
+++ real_code/frontend/www/js/main.js
@@ -17,14 +17,7 @@
 var basepath = "http://localhost/api/v1/db"
 var sports_mgr_hostname = "https://gh-sport-mgr-rz6q3h2zna-uc.a.run.app"
 
-// const searchBtn = document.getElementById('search-btn')
-// window.addEventListener('load', function(){
-//     searchBtn.addEventListener('click', function(){
-//         listTodos()
-//     })
-// })
-
-function listTodos() {
+function listData() {
     console.log("sending request to gh-sport-mgr")
     try {
         fetch('https://gh-sport-mgr-rz6q3h2zna-uc.a.run.app/api/v1/db', {
@@ -34,10 +27,42 @@ function listTodos() {
             }
         })
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => {
+            renderListData(data)
+        })
     } catch (error) {
         console.log(error)
     }
 }
 
+function renderListData(data) {
+    console.log(data)
+    const container = document.querySelector('.data-container')
+    container.innerHTML = ''
+    
+    data.forEach(element => {
+        const div = document.createElement('div')
+        div.classList.add('container')
+        const h2 = document.createElement('h2')
+        h2.classList.add('container-title')
+        h2.innerHTML = element.sport + ' Signup for '
+        const content = renderData(element)
 
+        div.appendChild(h2)
+        div.appendChild(content)
+
+        container.appendChild(div)
+    })
+}
+
+function renderData(element) {
+    const div = document.createElement('div')
+    div.classList.add('content')
+
+    const p = document.createElement('p')
+    p.innerHTML = 'Location: ' + element.location
+
+    div.appendChild(p)
+
+    return div
+}
