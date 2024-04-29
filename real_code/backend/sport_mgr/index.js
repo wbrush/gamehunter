81b0de949b702d5db9174baa968d184150e83cf4
Wayne Brush <wbrush@austin.rr.com>
2024-04-28 22:04:31 -0500

    Add CORS so the requests will work

--- real_code/backend/sport_mgr/index.js
+++ real_code/backend/sport_mgr/index.js
@@ -1,8 +1,14 @@
+// For more CORS information: https://stackabuse.com/handling-cors-with-node-js/ 
+// 
 const express = require("express")
 require("dotenv").config()
 
 const app = express()
 const port = process.env.PORT || 9000
+
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(port,()=>{
     console.log(`server listening on port ${port}`)
