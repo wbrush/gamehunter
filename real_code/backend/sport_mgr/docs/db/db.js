dd7e7b5d6db5a63de989124851b99bd0d60a0343
DBrush <coldnebraska@gmail.com>
2024-05-09 15:19:34 -0500

    updated read function (still timing out during connection)

--- real_code/backend/sport_mgr/docs/db/db.js
+++ real_code/backend/sport_mgr/docs/db/db.js
@@ -2,23 +2,23 @@ module.exports = { Read }
 
 async function Read(database) {
     const query = {
-        sql: 'SELECT SingerId, AlbumId, AlbumTitle FROM Albums',
+        sql: 'SELECT * FROM public.test',
     };
 
     // Queries rows from the Albums table
     try {
-        var numrows = 0
-        const [rows] = await database.run(query);
+        let numrows = 0
+        const response = await database.query(query.sql);
 
-        rows.forEach(row => {
-            const json = row.toJSON();
-            console.log(
-            `test: ${json.test}`
-            );
-            numrows = numrows+1
-        });
+        // rows.forEach(row => {
+        //     const json = row.toJSON();
+        //     console.log(
+        //     `test: ${json.test}`
+        //     );
+        //     numrows = numrows+1
+        // });
 
-        console.log("read ${numrows} of data")
+        console.log(`read ${response} of data`)
     } catch (err) {
         console.error('ERROR:', err);
     }
