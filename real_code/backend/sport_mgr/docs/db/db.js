56b3f9c1ac0c586524285ac7d3cb3d79f39613b8
DBrush <coldnebraska@gmail.com>
2024-05-13 11:06:06 -0500

    updated query to return row values

--- real_code/backend/sport_mgr/docs/db/db.js
+++ real_code/backend/sport_mgr/docs/db/db.js
@@ -2,24 +2,15 @@ module.exports = { Read }
 
 async function Read(database) {
     const query = {
-        sql: 'SELECT * FROM public.test',
-    };
+        sql: 'SELECT * FROM test',
+    }
 
     // Queries rows from the Albums table
     try {
-        let numrows = 0
-        const response = await database.query(query.sql);
+        const { rows } = await database.query(query.sql)
 
-        // rows.forEach(row => {
-        //     const json = row.toJSON();
-        //     console.log(
-        //     `test: ${json.test}`
-        //     );
-        //     numrows = numrows+1
-        // });
-
-        console.log(`read ${response} of data`)
-        return response
+        console.log(`read ${rows} of data`)
+        return rows
     } catch (err) {
         console.error('ERROR:', err);
         return err
