54205d6aacc662a703e9c35a7408ec27d1d9421e
DBrush <coldnebraska@gmail.com>
2024-10-28 15:13:13 -0500

    added base react app with routing set up

---
+++ real_code/frontend_react/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <Outlet />
+  );
+}
+
+export default App;
---
+++ real_code/frontend_react/src/main.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import NotFound from './pages/NotFound';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      } ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
