41b094a204d133bfa5432a95db0c80607a145935
DBrush <coldnebraska@gmail.com>
2024-10-28 18:20:38 -0500

    added search bar, react datepicker npm, and updated hero styling

--- real_code/frontend_react/src/pages/Search.jsx
+++ real_code/frontend_react/src/pages/Search.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import { Outlet } from 'react-router-dom'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
 import Header from '../components/header/header'
 import Modal from '../components/modal/modal'
 import Hero from '../components/hero/hero'
-import { Outlet } from 'react-router-dom'
+import '../pagescss/search.css'
 
 const Search = () => {
   const [modalVisibility, setModalVisibility ] = useState(false);
   const [modalDisplay, setModalDisplay] = useState('Login');
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
 
   try {
     fetch ('https://gh-sport-mgr-rz6q3h2zna-uc.a.run.app/api/v1/sport', {
@@ -33,18 +39,50 @@ const Search = () => {
 
         if (currentTimestamp < elementDate) {
           filteredEvents.push(element)
-        } //else {deleteQuery()}
+        } //else {deleteQuery()} deletes past events
     })
 
     console.log(filteredEvents)
   }
 
   return (
-    <div>
+    <div className='search-page'>
       <Header modalVisibility={modalVisibility} setModalVisibility={setModalVisibility} setModalDisplay={setModalDisplay} />
       <Modal modalVisibility={modalVisibility} setModalVisibility={setModalVisibility} modalDisplay={modalDisplay} setModalDisplay={setModalDisplay} />
 
       <Hero />
+
+      <div className="search-bar">
+        <div>
+          <p>Sport:</p>
+          <select>
+            <option>Volleyball</option>
+            <option>Basketball</option>
+            <option>Pickleball</option>
+            <option>Tennis</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Location:</p>
+          <select>
+            <option>Clay Madsen</option>
+            <option>Wells Branch</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Start Date:</p>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        </div>
+
+        <div>
+          <p>End Date:</p>
+          <DatePicker onChange={(date) => setEndDate(date)} />
+        </div>
+
+        <button>Search</button>
+      </div>
 
       <div className='search-container'>
         <Outlet />
