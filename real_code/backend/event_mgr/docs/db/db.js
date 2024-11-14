34e2f86baf1c151d02f1987096d8318476bc32af
DBrush <coldnebraska@gmail.com>
2024-11-14 16:11:45 -0600

    fixed syntax error with data structure

--- real_code/backend/event_mgr/docs/db/db.js
+++ real_code/backend/event_mgr/docs/db/db.js
@@ -16,9 +16,9 @@ async function Create(database, data) {
     }
 }
 
-async function Read(database, user_id) {
+async function Read(database, data) {
     let query = {
-        sql: `SELECT * FROM signedevents LEFT JOIN events ON signedevents.event_id = events.id WHERE user_id = ${user_id} ORDER BY date`
+        sql: `SELECT * FROM signedevents LEFT JOIN events ON signedevents.event_id = events.id WHERE user_id = ${data.user} ORDER BY date`
     }
 
     try {
