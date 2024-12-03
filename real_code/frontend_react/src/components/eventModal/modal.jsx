4a52d552a9bb831cc9bfd1515fbd69a48682ed83
DBrush <coldnebraska@gmail.com>
2024-12-03 12:45:52 -0600

    added error message toggle

--- real_code/frontend_react/src/components/eventModal/modal.jsx
+++ real_code/frontend_react/src/components/eventModal/modal.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+
 import Auth from '../../utils/auth'
 import { postEventRequest } from '../../utils/functions'
 
 import './modal.css';
 
 const EventModal = ({ modalVisibility, setModalVisibility, eventDetails}) => {
+    const [toggleError, setToggleError] = useState(false)
+    const [errorId, setErrorId] = useState('')
+
     const toggleModal = (event) => {
         if (event.target.className === 'event-modal' || event.target.className === 'close-modal') {
             setModalVisibility(!modalVisibility)
@@ -12,14 +17,21 @@ const EventModal = ({ modalVisibility, setModalVisibility, eventDetails}) => {
 
     const eventSignup = async (e) => {
         const userData = Auth.getUser()
-        const ids = {
-            user: userData.user.id,
-            event: Number(e.target.dataset.key)
-        }
 
-        const response = await postEventRequest('https://gh-event-mgr-462896897923.us-central1.run.app/api/v1/add/', ids)
-        if (response) {
-            setModalVisibility(false)
+        if (userData) {
+            setToggleError(false)
+            const ids = {
+                user: userData.user.id,
+                event: Number(e.target.dataset.key)
+            }
+
+            const response = await postEventRequest('https://gh-event-mgr-462896897923.us-central1.run.app/api/v1/add/', ids)
+            if (response) {
+                setModalVisibility(false)
+            }
+        } else {
+            setErrorId('added')
+            setToggleError(true)
         }
     }
 
@@ -34,6 +46,8 @@ const EventModal = ({ modalVisibility, setModalVisibility, eventDetails}) => {
                     <p>Facility: {eventDetails.location}</p>
 
                     <button data-key={eventDetails.id} onClick={eventSignup}>Add</button>
+
+                    {toggleError ? (<p className='error-msg' id={errorId}>Please login/signup to add events</p>) : null}
                 </div>
 
             </div>
