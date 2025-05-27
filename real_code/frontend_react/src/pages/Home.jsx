726172eca154de14a8cc95d5c24e9467a3830735
David Brush <brush.david99@gmail.com>
2025-05-27 15:52:02 -0500

    updated format for data fetch requests

--- real_code/frontend_react/src/pages/Home.jsx
+++ real_code/frontend_react/src/pages/Home.jsx
@@ -1,35 +1,15 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
 import Header from '../components/header/header';
 import Modal from '../components/userModal/modal';
 import Hero from '../components/hero/hero';
 import Calendar from '../components/calendar/calendar';
 
-import { filterData, getFetchRequest } from '../utils/functions';
-
 import '../pagescss/home.css'
 
 const Home = () => {
   const [modalVisibility, setModalVisibility ] = useState(false);
   const [modalDisplay, setModalDisplay] = useState('Login');
-
-  const initialMount = useRef(true)
-  const [dataReturned, setDataReturned ] = useState(null);
-
-  useEffect(() => {
-    if (initialMount.current) {
-      fetchRequest()
-
-      initialMount.current = false
-    } else {
-      filterData(dataReturned)
-    }
-  }, [dataReturned])
-
-  const fetchRequest = async () => {
-    const response = await getFetchRequest('https://gh-sport-mgr-rz6q3h2zna-uc.a.run.app/api/v1/sport')
-    setDataReturned(response)
-  }
 
   return (
     <div className='homepage'>
