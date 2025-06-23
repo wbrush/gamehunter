4d144b5f92bb84378db880384fde70a063ff2c63
David Brush <brush.david99@gmail.com>
2025-06-23 13:41:50 -0500

    updated create event section styling

--- real_code/frontend_react/src/components/createForm/form.jsx
+++ real_code/frontend_react/src/components/createForm/form.jsx
@@ -18,6 +18,7 @@ const Form = ({ form }) => {
     const [endMinute, setEndMinute] = useState(0)
     const [endCycle, setEndCycle] = useState('AM')
     const [creationFailure, setCreationFailure] = useState(false)
+    const [signupFailure, setSignupFailure] = useState(false)
     const [signupModal, setSignupModal] = useState(false)
     const [eventInfo, setEventInfo] = useState({
         id: 0,
@@ -37,6 +38,7 @@ const Form = ({ form }) => {
         } else {
             date.setHours(Number(startHour), Number(startMinute))
         }
+
         const tempStart = `${date.toUTCString()}`
         setStartDate(tempStart)
         
@@ -45,6 +47,7 @@ const Form = ({ form }) => {
         } else {
             date.setHours(Number(endHour), Number(endMinute))
         }
+
         const tempEnd = `${date.toUTCString()}`
         setEndDate(tempEnd)
         
@@ -67,6 +70,7 @@ const Form = ({ form }) => {
             } else {
                 eventTitle = 'Reserved Court'
             }
+
             setEventInfo({
                 id: result.response.rows[0].id,
                 startDate: tempStart,
@@ -82,7 +86,7 @@ const Form = ({ form }) => {
     const eventSignup = async () => {
         const userData = Auth.getUser()
         const data = {
-            user: userData.user.id,
+            user: userData?.user.id,
             event: eventInfo.id
         }
         const userEvents = JSON.parse(localStorage.getItem('user_events'))
@@ -95,12 +99,15 @@ const Form = ({ form }) => {
                 } else {
                     localStorage.setItem('user_events', JSON.stringify([eventInfo]))
                 }
+
                 setSignupModal(false)
 
                 // increment player count
                 const url = `https://gh-event-mgr-462896897923.us-central1.run.app/api/v1/inc/`
                 await postPlayerCountRequest(url, eventInfo.id)
             }
+        } else {
+            setSignupFailure(!signupFailure)
         }
     }
 
@@ -185,10 +192,13 @@ const Form = ({ form }) => {
                     <div className='create-modal'>
                         <h2>Event Created</h2>
                         <h4>Signup?</h4>
+
                         <div className="modal-buttons">
                             <p onClick={eventSignup}>Yes</p>
                             <p onClick={() => setSignupModal(false)}>No</p>
                         </div>
+                        
+                        {signupFailure ? <p id='error'>Please login to signup for event</p> : null}
                     </div>
                 ) : null}
             </form>
